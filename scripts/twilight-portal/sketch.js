1dceb17928400f9e15a741e803cb5da73a3f16bd
Alex <133706872+atpugh20@users.noreply.github.com>
2024-06-27 18:40:15 -0700

    1.7.6

---
+++ scripts/twilight-portal/sketch.js
@@ -0,0 +1,26 @@
+/* CANVAS SETUP */
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
+
+
+/* GLOBALS */
+
+/* MAIN */
+
+/* FUNCTIONS */
+
+function draw() {
+
+}
+
+function setup() {
+
+}
+
+function resizeCanvas() {
+
+}
+
+function clearCanvas() {
+
+}
