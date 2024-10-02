b44476c0d7f97c12a1202ce8124abaaad45f8764
Alex <133706872+atpugh20@users.noreply.github.com>
2024-10-02 04:10:24 -0700

    elastic collisions

---
+++ scripts/elastic-collisions/particle.js
@@ -0,0 +1,92 @@
+
+
+class Particle {
+  /**
+  * A particle is an object that moves around the canvas. The particles
+  * will bounce off of other objects, including the edges of the canvas.
+  */
+  constructor(x, y, color){
+    this.x = x;
+    this.y = y;
+    this.vel = {x: Math.random() * 3 - 1, y: Math.random() * 3 - 1};
+    this.acc = {x: 0, y: 0};
+    this.mass = Math.floor(Math.random() * 5 + 1);
+    this.radius = Math.sqrt(this.mass) * 5;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update(canvasWidth, canvasHeight) {
+    this.vel.x += this.acc.x;
+    this.vel.y += this.acc.y;
+    this.x += this.vel.x;
+    this.y += this.vel.y;
+    this.handleEdges(canvasWidth, canvasHeight);
+  }
+
+  collide(other) {
+    /**
+    * Handle when two particles collide with each other
+    */
+
+    // Calculate distance between two center points
+    const xDiff = (other.x - this.x) ** 2;
+    const yDiff = (other.y - this.y) ** 2;
+    let distance = Math.sqrt(Math.abs(xDiff + yDiff));
+    
+    if (distance < this.radius + other.radius) {      
+      let impact = {x: other.x - this.x, y: other.y - this.y}
+
+      // Overlap Correction
+      const overlap = distance - (this.radius + other.radius);
+      const angleAway = Math.atan2(impact.y, impact.x);
+      const newX = Math.cos(angleAway) * (overlap * 0.5);
+      const newY = Math.sin(angleAway) * (overlap * 0.5);
+      this.x += newX;
+      this.y += newY
+      other.x -= newX;
+      other.y -= newY;
+
+      // Perform calculation for both in this
+      
+      // Collision Calculation
+      distance = other.radius + this.radius;
+      impact = {x: other.x - this.x, y: other.y - this.y}
+      const mSum = this.mass + other.mass;
+      const velDiff = {x: other.vel.x - this.vel.x, y: other.vel.y - this.vel.y};
+      const dot = velDiff.x * impact.x + velDiff.y * impact.y;
+      // This particle
+      const den = mSum * distance * distance;
+      const deltaVA = {x: impact.x * (2 * other.mass * dot / den), y: impact.y * (2 * other.mass * dot / den)};
+      this.vel.x += deltaVA.x;
+      this.vel.y += deltaVA.y;
+      // Other particle
+      const deltaVB = {x: impact.x * (-2 * this.mass * dot / den), y: impact.y * (-2 *this.mass * dot / den)};
+      other.vel.x += deltaVB.x;
+      other.vel.y += deltaVB.y;
+    } 
+  }
+
+  handleEdges(canvasWidth, canvasHeight) {
+    if (this.x < this.radius) {
+      this.x = this.radius;
+      this.vel.x *= -1;
+    } else if (this.x > canvasWidth - this.radius) {
+      this.x = canvasWidth - this.radius;
+      this.vel.x *= -1;
+    }
+    if (this.y < this.radius) {
+      this.y = this.radius;
+      this.vel.y *= -1;
+    } else if (this.y > canvasHeight - this.radius) {
+      this.y = canvasHeight - this.radius;
+      this.vel.y *= -1;
+    }
+  }
+}
---
+++ scripts/elastic-collisions/sketch.js
@@ -0,0 +1,77 @@
+/** 
+* This project is based on Coding Train's "Elastic Collisions" coding 
+* challenge.
+*/
+
+/* CANVAS INIT */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const cW = canvas.width = window.innerWidth;
+const cH = canvas.height = window.innerHeight;
+canvas.style.backgroundColor = "black";
+
+/* GLOBALS */
+
+const FPS = 30;
+
+let frames = 0;
+let initTime = Date.now();
+let currentTime;
+
+const colors = ["red", "blue"];
+const particles = [];
+const numberOfParticles = Math.floor(cW * cH / 2000);
+
+/* FUNCTIONS */
+
+function rand(num) {
+  // Simplifies getting a random int between 0 - num
+  return Math.floor(Math.random() * num);
+}
+
+function clearCanvas(ctx) {  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function showStats() {
+  frames++;
+  currentTime = Date.now();
+  if (currentTime > initTime + 1000) {
+    console.log(frames);
+    frames = 0;
+    initTime = currentTime;
+  }
+}
+
+function setup() {
+  for (let i = 0; i < numberOfParticles; i++) {
+    particles.push(new Particle(rand(cW), rand(cH), colors[rand(colors.length)]));
+  }
+}
+
+function draw() {
+  /**
+  * This is the main canvas function. It is looped in the setInterval
+  * function. It updates each element, then draws them to the canvas.
+  */
+
+  showStats();
+  clearCanvas(ctx);
+  for (let i = 0; i < particles.length; i++) {
+    // Collision checks
+    for (let j = 0; j < particles.length; j++) {
+      if (i === j) continue;
+      particles[i].collide(particles[j]);
+    }
+    particles[i].update(cW, cH);
+    particles[i].draw(ctx);
+  }
+}
+
+/* MAIN */
+
+setup();
+setInterval(draw, 1000 / FPS);
